3bbe6516c20c734ca60252cc3bfc0701bcf19661
Sean <mushiwulf@gmail.com>
2015-02-16 10:11:40 -0800

    Add mocha test framework

--- blackjack.js
+++ blackjack.js
@@ -1,4 +1,4 @@
-$(function() {
+
     var deck = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
     function createShoe(deck, numberOfDecks) {
         var shoe = [];
@@ -51,4 +51,3 @@ $(function() {
     console.log(deck);
     testDeal();
 
-});
