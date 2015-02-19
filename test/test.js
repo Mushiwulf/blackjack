62ecbc7e2aa34eba54e3e1a8ac2ced1e666b071b
Sean <mushiwulf@gmail.com>
2015-02-18 19:41:03 -0800

    Add shuffle function

--- test/test.js
+++ test/test.js
@@ -23,23 +23,31 @@ describe('Blackjack Game', function(){
         })
     });
     describe('shuffleDeck', function(){
+        //need a before each?
         it('should return a shuffledShoe the same length as the shoe', function(){
-            assert.equal(makeShoe(1).length, shuffledShoe.length);
-            assert.equal(makeShoe(2).length, shuffledShoe.length);
+            assert.equal(makeShoe(1).length, shuffleShoe(makeShoe(1)).length);
+            assert.equal(makeShoe(2).length, shuffleShoe(makeShoe(2)).length);
         })
     })
 });
-var deck = [1,2,3,4,5,6,7,8,9,10,10,10,10
-    ,1,2,3,4,5,6,7,8,9,10,10,10,10
-    ,1,2,3,4,5,6,7,8,9,10,10,10,10
-    ,1,2,3,4,5,6,7,8,9,10,10,10,10];
-var shuffledShoe=[];
-shuffledShoe.length = 52;
+var deck = [11,2,3,4,5,6,7,8,9,10,10,10,10
+    ,11,2,3,4,5,6,7,8,9,10,10,10,10
+    ,11,2,3,4,5,6,7,8,9,10,10,10,10
+    ,11,2,3,4,5,6,7,8,9,10,10,10,10];
+var shoe = [];
+var shuffledShoe = [];
 var makeShoe = function(numberOfDecks) {
-    var shoe =[];
+    shoe =[];
     for (var i = 1; i <= numberOfDecks; i++) {
         shoe = shoe.concat(deck);
     }
 
     return shoe;
 };
+function shuffleShoe(shoe){
+    for(i=shoe.length;i >=0; i--){
+        var random = Math.floor(Math.random()*i);
+        shuffledShoe = shuffledShoe.concat(shoe.splice(random, 1));
+    }
+    return shuffledShoe;
+}
